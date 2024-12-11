0af9cad657343a3ea4be924446ff79cd606f3f57
gritsruslan <gritsruslan@gmail.com>
2024-12-11 19:47:50 +0300

    add list implementation

---
+++ src/DataStructures/List/MyList.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+
+namespace DataStructures.List;
+
+public class MyList<T> : IList<T>
+{
+	private const int DefaultCapacity = 5;
+
+	private T[] _data;
+
+	public int Count { get; private set; }
+
+	public int Capacity => _data.Length;
+
+	public bool IsReadOnly => false;
+
+	public T this[int index]
+	{
+		get
+		{
+			ThrowIfIndexOutOfRange(index);
+			return _data[index];
+		}
+		set
+		{
+			ThrowIfIndexOutOfRange(index);
+			_data[index] = value;
+		}
+	}
+
+	public MyList(int capacity)
+	{
+		ArgumentOutOfRangeException.ThrowIfNegativeOrZero(capacity);
+		_data = new T[capacity];
+	}
+
+	public MyList(IEnumerable<T> enumerable)
+	{
+		if (enumerable is ICollection<T> collection)
+		{
+			int colCount = collection.Count;
+
+			if (collection.Count == 0)
+				_data = [];
+			else
+			{
+				_data = new T[colCount];
+				collection.CopyTo(_data, 0);
+			}
+		}
+		else
+		{
+			_data = new T[DefaultCapacity];
+
+			using var enumerator = enumerable.GetEnumerator();
+
+			while(enumerator.MoveNext())
+				Add(enumerator.Current);
+		}
+	}
+
+	public void Add(T item)
+	{
+		if(Count == Capacity)
+			ResizeArray();
+
+		_data[Count] = item;
+		Count++;
+	}
+
+	public void Clear()
+	{
+		_data = new T[DefaultCapacity];
+		Count = 0;
+	}
+
+	public bool Contains(T item) => IndexOf(item) != -1;
+
+	public void CopyTo(T[] array, int arrayIndex)
+	{
+		if (arrayIndex < 0 || arrayIndex >= array.Length)
+			throw new ArgumentOutOfRangeException();
+
+		if (array.Rank > 1)
+			throw new ArgumentException();
+
+		int arrayCapacity = array.Length - arrayIndex - 1;
+
+		if(arrayCapacity < Count)
+			throw new ArgumentException();
+
+		for (int i = 0; i < Count; i++)
+		{
+			int currentArrayIndex = arrayIndex + i;
+
+			array[currentArrayIndex] = _data[i];
+		}
+	}
+
+	public bool Remove(T item)
+	{
+		int index = IndexOf(item);
+
+		if (index == -1)
+			return false;
+
+		RemoveAt(index);
+
+		return true;
+	}
+
+	public int IndexOf(T item)
+	{
+		for (int i = 0; i < Count; i++)
+		{
+			var currentItem = _data[i];
+
+			if (currentItem == null)
+			{
+				if (item == null)
+					return i;
+
+				continue;
+			}
+
+			if (currentItem.Equals(item))
+				return i;
+		}
+
+		return -1;
+	}
+
+	public void Insert(int index, T item)
+	{
+		ThrowIfIndexOutOfRange(index);
+
+		if(Count == Capacity)
+			ResizeArray();
+
+		for (int i = Count; i > index; i--)
+		{
+			_data[i] = _data[i - 1];
+		}
+
+		_data[index] = item;
+		Count++;
+	}
+
+	public void RemoveAt(int index)
+	{
+		ThrowIfIndexOutOfRange(index);
+
+		for (int i = index + 1; i < Count; i++)
+		{
+			_data[i - 1] = _data[i];
+		}
+
+		Count--;
+	}
+
+	private void ResizeArray()
+	{
+		if (Capacity == int.MaxValue)
+			throw new Exception();
+
+		uint newArraySize = (uint)(Capacity + Math.Round(Capacity / 2.0));
+
+		if (newArraySize > Array.MaxLength)
+			newArraySize = (uint) Array.MaxLength;
+
+		T[] newData = new T[newArraySize];
+
+		Array.Copy(_data, newData, Count);
+		_data = newData;
+	}
+	public IEnumerator<T> GetEnumerator() => new MyListEnumerator(this);
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	private void ThrowIfIndexOutOfRange(int index)
+	{
+		if (index < 0 || index >= Count)
+			throw new IndexOutOfRangeException();
+	}
+
+	private struct MyListEnumerator : IEnumerator<T>
+	{
+
+		public MyListEnumerator(MyList<T> list)
+		{
+			_list = list;
+		}
+
+		private MyList<T> _list;
+
+		private int _index = 0;
+		public T Current
+		{
+			get
+			{
+				if (_index < 0 || _index >= _list.Count)
+					throw new InvalidOperationException();
+
+				return _list[_index];
+			}
+		}
+
+		object IEnumerator.Current => Current!;
+
+		public bool MoveNext()
+		{
+			_index++;
+			return _index < _list.Count;
+		}
+
+		public void Reset()
+		{
+			_index = -1;
+		}
+
+		public void Dispose() { }
+	}
+
+	public static implicit operator MyList<T>(T[] enumerable) => new(enumerable);
+}
